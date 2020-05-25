5116425ae20a084e80a9e889565a8723aac6ce9e
Andrew Mason <andrewmasonmedia@gmail.com>
2020-05-25 17:36:26 -0400

    Image updates for decentral page

--- pages/projects/decentral.js
+++ pages/projects/decentral.js
@@ -6,11 +6,11 @@ import ProjectLinks from '../../components/ProjectLinks';
 import { motion } from 'framer-motion';
 import * as Constants from '../../components/Constants';
 import Footer from '../../components/footer';
-import jaxx_2 from '../../images/jaxx/jaxx_laptop_coffee.jpg';
-import jaxx_3 from '../../images/jaxx/jaxx_multi.jpg';
-import jaxx_4 from '../../images/jaxx/jaxx_ipad.jpg';
-import jaxx_5 from '../../images/jaxx/jaxx_2phones.jpg';
-import jaxx_6 from '../../images/jaxx/jaxx_grafitti.jpg';
+import dc_1 from '../../images/dc/dc_laptop.png';
+import dc_2 from '../../images/dc/jaxx_imac.jpg';
+import dc_3 from '../../images/dc/decentral_flatiron.jpeg';
+import dc_4 from '../../images/dc/jaxx_hand_phone.jpg';
+import dc_5 from '../../images/dc/jaxx_ipad_iphone.jpg';
 
 export class decentral extends Component {
     componentDidMount() {
@@ -33,18 +33,18 @@ export class decentral extends Component {
                     projectLetter="DC"
                     projectTitle="Decentral Inc."
                     projectDesc="Jaxx Liberty is a cryptocurrency wallet app made by Decentral Inc. In my role as Decentral's lead website designer/developer, I completely redesigned the Jaxx Liberty public facing website, modernized its branding, layout, and color palette, significantly improved SEO and engagement, and created hundreds of image assets. I also redesigned their Wordpress blog and customer support website (Zendesk). In this process, I revitalized the Jaxx brand and helped increase user base and web presence."
-                    projectImg1={jaxx_5}
-                    projectImg2={jaxx_2}
+                    projectImg1={dc_1}
+                    projectImg2={dc_2}
                     projecSubTitle2="Jaxx Liberty"
                     projectTitle2="Tech Stack"
                     projectDesc2="I coded the main product website using HTML5, PHP, CSS3, SWUP, GSAP, and included a Zendesk help widget for support functionality and a Cookie Consent/GDPR plugin by Insites. I also duplicated the site as a single page application using Angular 7/8, and incorporated market data, blockchain news, Zendesk support FAQ, and Jaxx blog APIs to reproduce the functionality of the Jaxx Liberty mobile app. 
                     *I do not maintain the current jaxx.io website. For confidentiality, the codebase link below is to an earlier version of jaxx.io, still designed and built by me."
                     projectDescCol="bg-yellow"
-                    projectImg3={jaxx_6}
+                    projectImg3={dc_3}
                     imgClass3="bg-purple"
-                    projectImg4={jaxx_3}
+                    projectImg4={dc_4}
                     imgClass4="bg-pink"
-                    projectImg5={jaxx_4}
+                    projectImg5={dc_5}
                     imgClass5="bg-pink"
                     >
                         <ProjectLinks 
