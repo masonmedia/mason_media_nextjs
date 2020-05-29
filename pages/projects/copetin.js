6da0c130c063f1d7327fef4cae555c5218dc1733
Andrew Mason <andrewmasonmedia@gmail.com>
2020-05-29 16:04:48 -0400

    Copy updates to Copetin project

--- pages/projects/copetin.js
+++ pages/projects/copetin.js
@@ -17,7 +17,7 @@ export class copetin extends Component {
             metaTitle="Copetín"
             metaD="Website coding, graphic/UI design."
             projectNumber="06."
-            numberClass="bg-success text-light"
+            numberClass="bg-navy text-light"
             projectLetter="copetin"
             projectTitle="Copetín"
             projectDesc="This project was a website mockup-makeover for Copetín, the now defunct gourmet restaurant of Toronto celebrity Chef Claudio Aprile. The project redesigned Copetín's minimal landing-page website to offer a much bolder, more modern layout, while expanding on the restaurant's existing branding, logo, and color themes. Goals were to take their existing content and elevate it into an elegant, stylish, one-page website that matched the restaurant's cachet and haute cuisine."
@@ -25,18 +25,19 @@ export class copetin extends Component {
             projectImg2={copetin_3}
             projecSubTitle2="Copetín"
             projectTitle2="Tech Stack"
-            projectDesc2="The design solution consisted of a responsive one-page layout with image carousel, full menu presentation, Open Table reservation API, social media integration, hours and contact information, with branding and colour themes extended creatively throughout. The site was built with HTML5, CSS3, Bootstrap 3, Flexbox, and Javascript for smooth anchor scrolling, and incorporated custom typography. While the project didn't move forward, it was a huge improvement on their existing site and was instructive in designing for the restaurant industry."
-            projectDescCol="bg-black text-light"
+            projectDesc2="The site was built with HTML5, CSS3, Bootstrap 3, Flexbox, and Javascript. It features a responsive one-page layout with smooth anchor scrolling menu, image carousel, full restaurant menu presentation, Open Table reservation API, social media integration, hours and contact information, custom font, with branding and colour themes extended creatively throughout. While the project didn't move forward, it was a huge improvement on their existing site and was instructive in designing for the restaurant industry."
+            projectDescCol="bg-navy text-light"
             projectImg3={copetin_4}
             projectImg4={copetin_1}
             projectImg5={copetin_5}
             >
                 <ProjectLinks 
                 projectTitle="Decentral Inc."
-                githubUrl="https://github.com/masonmedia/big-horn-sheep"
-                behanceUrl="https://www.behance.net/gallery/97962231/Big-Horn-Sheep"
-                designCircle="•"
-                siteUrl="http://bighornsheepband.com/"
+                githubUrl="https://github.com/masonmedia/copetin"
+                // behanceUrl="https://www.behance.net/gallery/97962231/Big-Horn-Sheep"
+                behanceClass="d-none"
+                // designCircle="•"
+                siteUrl="https://copetin.andrewmasonmedia.com/"
                 prevHref="/projects/sfs"
                 nextHref="/projects"
                 />
