89a533e6543496d58280db39cb3f7fdbc818d811
Andrew Mason <andrewmasonmedia@gmail.com>
2020-05-30 22:19:50 -0400

    Updates to writing page

--- pages/writing.js
+++ pages/writing.js
@@ -50,7 +50,7 @@ export class writing extends Component {
                     <Row className="bg-smoke">
                         <Col xl={12} className="align-left min-100 p-5">
                         <motion.div variants={Constants.textVariants}>
-                            <h6 className="text-uppercase rainbow-text-2 mt-5 pt-2">Content is king</h6>
+                            <h6 className="text-uppercase rainbow-text-2 mt-5 pt-2">Overview</h6>
                             <h1 className="page_title relative rainbow-text-2 pb-3 mb-0">I write and edit copy for a wide variety of contexts. I strive for fast moving, easy-to-read content with humour and originality, while specializing in UX, SEO, and editing for web presentation.
                             </h1>
                             <div className="page_title rainbow-text-2 font-weight-light">↓</div>
@@ -60,7 +60,7 @@ export class writing extends Component {
                     
                     <Row>
                         <Col xl={12} className="bg-black text-light align-left min-100 p-5">
-                            <h6 className="text-uppercase">Content</h6>
+                            <h6 className="text-uppercase">Copywriting</h6>
                             <h1 className="page_title font-weight-bold ">I have broad experience with short and long form writing on a range of topics from music, to fashion, fitness, food/cooking, lifestyle, design, real estate, and business.</h1>
                             <div className="display-3 font-weight-light">↓</div>
                         </Col>
@@ -68,7 +68,7 @@ export class writing extends Component {
 
                     <Row>
                         <Col xl={12} className="bg-smoke align-left min-100 p-5">
-                            <h6 className="text-uppercase">Frameworks + Animation</h6>
+                            <h6 className="text-uppercase">Content Creation</h6>
                             <h1 className="page_title font-weight-bold ">I create a range of different content types, from articles, to reviews, blog and social media posts, poster/flyer copy, bios, and album/track descriptions all written with a fast, fluid style.</h1>
                             <div className="display-3 font-weight-light">↓</div>
                         </Col>
