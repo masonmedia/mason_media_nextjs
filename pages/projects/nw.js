ad862eed91259477a7f3e38cdf2d5762428af683
Andrew Mason <andrewmasonmedia@gmail.com>
2020-06-14 10:28:53 -0400

    Updates to neil whitford page copy and images

--- pages/projects/nw.js
+++ pages/projects/nw.js
@@ -4,7 +4,7 @@ import ProjectLinks from '../../components/ProjectLinks';
 import nw_1 from '../../public/images/nw/nw_macbook.png';
 import nw_2 from '../../public/images/nw/nw_iphone8_home.png';
 import nw_3 from '../../public/images/nw/nw_macbook_guitarist.png';
-import nw_4 from '../../public/images/nw/nw_iphone7disco.png';
+import nw_4 from '../../public/images/nw/nw_iphone5disco.png';
 import nw_5 from '../../public/images/nw/nw_iphone7.png';
 
 export class nw extends Component {
@@ -19,7 +19,7 @@ export class nw extends Component {
             numberClass="bg-neil"
             projectLetter="NW"
             projectTitle="Neil Whitford"
-            projectDesc="Advance Therapy's public facing website required a complete overhaul and redesign. Requirements included a simple, intuitive user interface, a low amount of scrolling while still presenting a large amount of content, a specific typeface, and the firm's existing logo/color palette. I went with a simple one-page layout centering on jQuery clickable dropdown panels which revealed text for each subsection, with futher features including flexbox construction, a side-canvas menu, smooth anchor scrolling, and image sourcing."
+            projectDesc="Neil Whitford is a Toronto musician, composer, producer and educator. He required a public facing website for his personal brand as well as production and teaching businesses. The project required UI/UX and graphic design, branding, as well as copy editing. The end result included a bold palette of neon colors on a stark black background, a simple but modern Helvetica typeface, animations, and presentation of his library of audio tracks and a complete discography."
             projectImg1={nw_1}
             imgColClass1="bg-pink"
             imgClass1="drop-shadow p-2"
@@ -27,14 +27,14 @@ export class nw extends Component {
             imgColClass2="bg-squares"
             projecSubTitle2="Advance Therapy"
             projectTitle2="tech stack"
-            projectDesc2="I redesigned the site again recently to modernize and expant the layout as well as add new content. Having worked with React on numerous recent projects I was tired of the tooling and setup, so built a similar component-based architecture using PHP. The site employs reusable HTML components with PHP variables that allow for SPA-style data binding customization. The site also uses SWUP, SASS, Bootstrap, AOS, Undraw illustations, and an expanded gradient color palette. Advance also required UX copywriting and editing."
+            projectDesc2="I built the site using HTML5, PHP, CSS3, Bootstrap 4, SWUP, AOS, and the parallax animation  library rellax.js. It also includes audio player embeds from Souncloud and hearthis.at platforms."
             projectDescCol="bg-black text-light"
             projectImg3={nw_3}
             imgColClass3="bg-neil"
             imgClass3="drop-shadow p-2"
             projectImg4={nw_4}
             imgColClass4="bg-yellow"
-            imgClass4="drop-shadow"
+            imgClass4=""
             projectImg5={nw_5}
             imgClass5="drop-shadow p-2"
               >
